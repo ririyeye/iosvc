044a06723b6e4dabc7826a798adca1ba27bfb49d
ririyeye <200610237@qq.com>
2020-12-26 23:29:06 +0800

    123

--- main.cpp
+++ main.cpp
@@ -26,64 +26,64 @@ boost::shared_ptr<std::string> make_daytime_string()
 	return boost::make_shared<std::string>(ctime(&now));
 }
 
-class udp_server
-{
-public:
-  udp_server(boost::asio::io_context& io_context)
-    : socket_(io_context, udp::endpoint(udp::v4(), 13))
-  {
-    start_receive();
-  }
+class udp_server {
+    public:
+	udp_server(boost::asio::io_context &io_context)
+		: socket_(io_context, udp::endpoint(udp::v4(), 13))
+	{
+		start_receive();
+	}
 
-private:
-  void start_receive()
-  {
-    socket_.async_receive_from(
-        boost::asio::buffer(recv_buffer_), remote_endpoint_,
-        boost::bind(&udp_server::handle_receive, this,
-          boost::asio::placeholders::error,
-          boost::asio::placeholders::bytes_transferred));
-  }
+    private:
+	void start_receive()
+	{
+		socket_.async_receive_from(
+			boost::asio::buffer(recv_buffer_), remote_endpoint_,
+			boost::bind(
+				&udp_server::handle_receive, this,
+				boost::asio::placeholders::error,
+				boost::asio::placeholders::bytes_transferred));
+	}
 
-  void handle_receive(const boost::system::error_code& error,
-      std::size_t /*bytes_transferred*/)
-  {
-    if (!error)
-    {
-      boost::shared_ptr<std::string> message(make_daytime_string());
+	void handle_receive(const boost::system::error_code &error,
+			    std::size_t /*bytes_transferred*/)
+	{
+		if (!error) {
+			boost::shared_ptr<std::string> message(
+				make_daytime_string());
 
-      socket_.async_send_to(boost::asio::buffer(*message), remote_endpoint_,
-          boost::bind(&udp_server::handle_send, this, message,
-            boost::asio::placeholders::error,
-            boost::asio::placeholders::bytes_transferred));
+			socket_.async_send_to(
+				boost::asio::buffer(*message), remote_endpoint_,
+				boost::bind(&udp_server::handle_send, this,
+					    message,
+					    boost::asio::placeholders::error,
+					    boost::asio::placeholders::
+						    bytes_transferred));
 
-      start_receive();
-    }
-  }
+			start_receive();
+		}
+	}
 
-  void handle_send(boost::shared_ptr<std::string> /*message*/,
-      const boost::system::error_code& /*error*/,
-      std::size_t /*bytes_transferred*/)
-  {
-  }
+	void handle_send(boost::shared_ptr<std::string> /*message*/,
+			 const boost::system::error_code & /*error*/,
+			 std::size_t /*bytes_transferred*/)
+	{
+	}
 
-  udp::socket socket_;
-  udp::endpoint remote_endpoint_;
-  boost::array<char, 1> recv_buffer_;
+	udp::socket socket_;
+	udp::endpoint remote_endpoint_;
+	boost::array<char, 1> recv_buffer_;
 };
 
 int main()
 {
-  try
-  {
-    boost::asio::io_context io_context;
-    udp_server server(io_context);
-    io_context.run();
-  }
-  catch (std::exception& e)
-  {
-    std::cerr << e.what() << std::endl;
-  }
+	try {
+		boost::asio::io_context io_context;
+		udp_server server(io_context);
+		io_context.run();
+	} catch (std::exception &e) {
+		std::cerr << e.what() << std::endl;
+	}
 
-  return 0;
+	return 0;
 }
